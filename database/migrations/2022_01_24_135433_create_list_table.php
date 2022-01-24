0fc7ef31d6d3e9db337da9f1280ac157ac3bca1d
“Cal-107” <kaly___7@hotmail.it>
2022-01-24 15:21:49 +0100

    first commit

---
+++ database/migrations/2022_01_24_135433_create_list_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination', 80);
+            $table->string('airport', 80);
+            $table->tinyInteger('flight_hours');
+            $table->string('departure', 20);
+            $table->string('arrival', 20);
+            $table->boolean('shuttle_service', 80);
+            $table->boolean('hotel');
+            $table->unsignedTinyInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list');
+    }
+}
