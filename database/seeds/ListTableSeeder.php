2585c64cc59e0364fb2b34e94f87888b326e5941
“Cal-107” <kaly___7@hotmail.it>
2022-01-24 16:06:52 +0100

    add seeder and fake

---
+++ database/seeds/ListTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Journey;
+
+
+class ListTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i  < 10; $i++) { 
+            $new_journey = new Journey();
+            $new_journey->destination = $faker->text('50');
+            $new_journey->airport = $faker->text('50');
+            $new_journey->flight_hours = $faker->randomNumber(4, false) . ' Hours';
+            $new_journey->departure = $faker->text('50');
+            $new_journey->arrival = $faker->text('50');
+            $new_journey->shuttle_service = $faker->boolean();
+            $new_journey->hotel = $faker->boolean();
+            $new_journey->destination = $faker->randomNumber(3, true) . '$';
+            $new_journey->save();
+        }
+    }
+}
